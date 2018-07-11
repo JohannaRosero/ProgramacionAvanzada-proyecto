2c7a23fe368b7c3646ad586da52eedbbd48341df
JohannaRosero <30345783+JohannaRosero@users.noreply.github.com>
2018-07-11 01:18:07 -0500

    cambios

---
+++ tictac/servidorJuego.js
@@ -0,0 +1,46 @@
+
+    var express= require('express'),
+    aplicacion = express(),
+    server = require('http').createServer(aplicacion),
+    io = require('socket.io').listen(server);
+	console.log("jugada1");
+	var colores = new Array(36);
+	for (var i = 1; i < 37; i++) {
+		colores[i]=0;
+			   
+	}
+
+    server.listen(3000);
+	console.log("jugada2");
+
+    //renderizar
+    aplicacion.get("/",function(req,res){
+        res.sendFile(__dirname+'/juego.html');
+		console.log("jugada3");
+    });
+
+
+ //se recoge el dato que se encuentra en el sockets y se envía a los demás
+   console.log("esperando conexion"); 
+   io.sockets.on('connection',function(socket){
+            console.log("conexion Establecida");           
+     
+ socket.on('jugada',function(dataS){
+		colores[dataS.id2]=1;
+		io.sockets.emit('jugada2',dataS);
+		for (var i = 1; i < 37; i++) {
+			if(colores[i]==1)
+			{
+				dataS.id2=i;
+				io.sockets.emit('jugada2',dataS);
+			}		   
+		}
+       
+       console.log("jugada"+dataS.id+"--"+dataS.id2);
+ });
+   });
+
+
+    
+
+
