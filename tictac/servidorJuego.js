f9b27748c6b2f75c0eeded6bd2c2fb15d4570bcd
JohannaRosero <30345783+JohannaRosero@users.noreply.github.com>
2018-07-11 06:13:48 -0500

    final

--- tictac/servidorJuego.js
+++ tictac/servidorJuego.js
@@ -5,6 +5,7 @@
     io = require('socket.io').listen(server);
 	console.log("jugada1");
 	var colores = new Array(36);
+	var flag = 0;
 	for (var i = 1; i < 37; i++) {
 		colores[i]=0;
 			   
@@ -18,9 +19,17 @@
 
    console.log("esperando conexion"); 
    io.sockets.on('connection',function(socket){
-            console.log("conexion Establecida");   
-     
+            console.log("conexion Establecida");
+			if(flag==1){
+				for (var i = 1; i < 37; i++) {
+					if(colores[i]==1)
+					{
+						io.sockets.emit('actualiza',i);
+					}		   
+				}
+			}
  socket.on('jugada',function(dataS){
+		flag =1;
 		colores[dataS.id2]=1;
 		io.sockets.emit('jugada2',dataS);
 		for (var i = 1; i < 37; i++) {
